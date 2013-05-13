869a7b76cbbe0818cde3331dd687f7e84d0e6fd0
Tony Morris <tmorris@tmorris.net>
2013-05-13 20:45:29 +1000

    testing

--- src/test/scala/workshop/parser/NotationAwesomeArbitrary.scala
+++ src/test/scala/workshop/parser/NotationAwesomeArbitrary.scala
@@ -5,28 +5,22 @@ import org.scalacheck.{Arbitrary, Gen}, Arbitrary._, Gen._
 
 object NotationAwesomeArbitrary {
   implicit def ArbitraryNotationAwesome: Arbitrary[NotationAwesome] = {
-    val bool: Gen[NotationAwesome] = arbitrary[Boolean] map {
+    def bool: Gen[NotationAwesome] = arbitrary[Boolean] map {
       case true => AwesomeTrue
       case false => AwesomeFalse
     }
 
-    val string: Gen[NotationAwesome] = arbitrary[String] map AwesomeString
+    def string: Gen[NotationAwesome] = arbitrary[String] map AwesomeString
 
-    def totally: Gen[NotationAwesome] = listOf(awesome) map TotallyAwesome
+    def totally: Gen[NotationAwesome] =
+      for {
+        n <- choose(0, 9)
+        t <- listOfN(n, awesome)
+      } yield TotallyAwesome(t)
 
-    def awesome: Gen[NotationAwesome] = oneOf(bool, string, totally)
+    def awesome: Gen[NotationAwesome] =
+      frequency((2, bool), (2, string), (1, totally))
 
     Arbitrary(awesome)
   }
-
-  /*
-  at workshop.parser.NotationAwesomeArbitrary$$anonfun$totally$1$1.apply(NotationAwesomeArbitrary.scala:15)
-  at workshop.parser.NotationAwesomeArbitrary$$anonfun$totally$1$1.apply(NotationAwesomeArbitrary.scala:15)
-  at org.scalacheck.Gen$.listOf(Gen.scala:340)
-  at workshop.parser.NotationAwesomeArbitrary$.totally$1(NotationAwesomeArbitrary.scala:15)
-  at workshop.parser.NotationAwesomeArbitrary$.workshop$parser$NotationAwesomeArbitrary$$awesome$1(NotationAwesomeArbitrary.scala:17)
-  at workshop.parser.NotationAwesomeArbitrary$$anonfun$totally$1$1.apply(NotationAwesomeArbitrary.scala:15)
-  at workshop.parser.NotationAwesomeArbitrary$$anonfun$totally$1$1.apply(NotationAwesomeArbitrary.scala:15)
-  at org.scalacheck.Gen$.listOf(Gen.scala:340)
-  */
 }
--- src/test/scala/workshop/parser/WorkshopSpec.scala
+++ src/test/scala/workshop/parser/WorkshopSpec.scala
@@ -3,56 +3,57 @@ package parser
 
 object WorkshopSpec extends Spec {
   import NotationAwesome._
+  "parser" should {
+    "sole true" in {
+      awesomeP parse "true" must_== ParseValue(AwesomeTrue)
+    }
 
-  "sole true" should {
-    awesomeP parse "true" must_== ParseValue(AwesomeTrue)
-  }
+    "list with single value" in {
+      awesomeP parse "[false]" must_== ParseValue(TotallyAwesome(List(AwesomeFalse)))
+    }
 
-  "list with single value" should {
-    awesomeP parse "[false]" must_== ParseValue(TotallyAwesome(List(AwesomeFalse)))
-  }
+    "empty list" in {
+      awesomeP parse "[]" must_== ParseValue(TotallyAwesome(List()))
+    }
 
-  "empty list" should {
-    awesomeP parse "[]" must_== ParseValue(TotallyAwesome(List()))
-  }
+    "sole string" in {
+      awesomeP parse "\"awesome\"" must_== ParseValue(AwesomeString("awesome"))
+    }
 
-  "sole string" should {
-    awesomeP parse "\"awesome\"" must_== ParseValue(AwesomeString("awesome"))
-  }
+    "sole string containing brackets" in {
+      awesomeP parse "\"awe[some]\"" must_== ParseValue(AwesomeString("awe[some]"))
+    }
 
-  "sole string containing brackets" should {
-    awesomeP parse "\"awe[some]\"" must_== ParseValue(AwesomeString("awe[some]"))
-  }
+    "list with several top-level values" in {
+      awesomeP parse "[\"awesome\", false, true]" must_== ParseValue(TotallyAwesome(List(AwesomeString("awesome"), AwesomeFalse, AwesomeTrue)))
+    }
 
-  "list with several top-level values" should {
-    awesomeP parse "[\"awesome\", false, true]" must_== ParseValue(TotallyAwesome(List(AwesomeString("awesome"), AwesomeFalse, AwesomeTrue)))
-  }
+    "list with embedded values" in {
+      awesomeP parse "[\"awesome\", false, [ true, false, \"more awesome\"]]" must_== ParseValue(TotallyAwesome(List(AwesomeString("awesome"), AwesomeFalse, TotallyAwesome(List(AwesomeTrue, AwesomeFalse, AwesomeString("more awesome"))))))
+    }
 
-  "list with embedded values" should {
-    awesomeP parse "[\"awesome\", false, [ true, false, \"more awesome\"]]" must_== ParseValue(TotallyAwesome(List(AwesomeString("awesome"), AwesomeFalse, TotallyAwesome(List(AwesomeTrue, AwesomeFalse, AwesomeString("more awesome"))))))
-  }
+    "list with values interspersed with whitespace" in {
+    awesomeP parse " [ \"awesome\" , false, [ true , false, \"more awesome\"] ] " must_== ParseValue(TotallyAwesome(List(AwesomeString("awesome"), AwesomeFalse, TotallyAwesome(List(AwesomeTrue, AwesomeFalse, AwesomeString("more awesome"))))))
+    }
 
-  "list with values interspersed with whitespace" should {
-    awesomeP parse "  [  \"awesome\"  , false, [ true   , false, \"more awesome\"]  ]  " must_== ParseValue(TotallyAwesome(List(AwesomeString("awesome"), AwesomeFalse, TotallyAwesome(List(AwesomeTrue, AwesomeFalse, AwesomeString("more awesome"))))))
-  }
+    "arbitrary characters fail" in {
+      awesomeP.parse("awesome").isFail
+    }
 
-  "arbitrary characters fail" should {
-    awesomeP.parse("awesome").isFail
-  }
+    "unbalanced quotes fail" in {
+      awesomeP.parse("\"awesome").isFail
+    }
 
-  "unbalanced quotes fail" should {
-    awesomeP.parse("\"awesome").isFail
-  }
+    "unbalanced left bracket fail" in {
+      awesomeP.parse("[ true").isFail
+    }
 
-  "unbalanced left bracket fail" should {
-    awesomeP.parse("[ true").isFail
-  }
+    "unbalanced right bracket fail" in {
+      awesomeP.parse("], ").isFail
+    }
 
-  "unbalanced right bracket fail" should {
-    awesomeP.parse("],  ").isFail
-  }
-
-  "misplaced comma fail" should {
-    awesomeP.parse(",  ").isFail
+    "misplaced comma fail" in {
+      awesomeP.parse(", ").isFail
+    }
   }
 }
